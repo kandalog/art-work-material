b3d4f9783218a32b82e98c0d2bde43085dcf1473
kandalog <asuasu.tee@gmail.com>
2024-01-10 21:46:32 +0900

    basic情報追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,95 @@ export default function Home() {
   return (
     <>
       <Navigation />
+      <div className="shadow-custom p-1">
+        # Miracleman <br />
+        Alan Moore
+        <br />
+        <br />
+        # The Saga of the Swamp Thing
+        <br />
+        Alan Moore
+        <br />
+        <br />
+        # Watchmen
+        <br />
+        脚本: Alan Moore
+        <br />
+        作画: Dave Gibbons
+        <br />
+        <br />
+        # The Killing Joke (バットマン)
+        <br />
+        脚本: Alan Moore
+        <br />
+        作画: Brian Bolland
+        <br />
+        Barbara Gordon(別名Batgirl)を撃ち彼女は腰から下が麻痺する
+        <br />
+        <br />
+        # V for Vendetta
+        <br />
+        脚本: Alan Moore
+        <br />
+        作画: David Lloyd
+        <br />
+        <br />
+        # From Hell
+        <br />
+        脚本: Alan Moore
+        <br />
+        作画: Eddie Campbell
+        <br />
+        <br />
+        # The League of Extraordinary Gentlemen
+        <br />
+        脚本: Alan Moore
+        <br />
+        作画: Kevin O'Neill
+        <br />
+        "A graphic novel series depicting a superhero team based on fictional
+        characters from the Victorian age literature."
+        <br />
+        <br />
+        # A Death in the Family (バッドマン)
+        <br />
+        担当者: "Marv Wolfman"と"Jim Aparo"
+        <br />
+        バッドマンのサイドキックRobinの死が電話投票で決定した
+        <br />
+      </div>
+
+      <div className="my-2 shadow-custom p-1">
+        # Daredevil
+        <br />
+        担当者: Gen Colan降板後はFlank Miller
+        <br />
+        <br />
+        # Wolverine
+        <br />
+        脚本: Chris Claremont
+        <br />
+        作画: Flank Miller
+        <br />
+        <br />
+        # Ronin 担当者: Flank Miller
+        <br />
+        <br />
+        # The Dark Night Returns (バッドマン Jokerの死とsupermanとの決闘)
+        <br />
+        担当者: Flank Miller
+        <br />
+        <br />
+        # Shin City
+        <br />
+        作者: Flank Miller
+        <br />
+        <br />
+        # 300
+        <br />
+        作者 Flank Miller
+        <br />
+      </div>
     </>
   );
 }
